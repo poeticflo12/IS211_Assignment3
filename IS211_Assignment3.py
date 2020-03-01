df3542e6f0608657857c14adf99e5cbeec4067fe
poeticflo12 <gelaneymatthew@gmail.com>
2020-03-01 08:06:05 -0500

    final

--- IS211_Assignment3.py
+++ IS211_Assignment3.py
@@ -8,7 +8,6 @@ import csv
 import re
 
 
-
 # This reads and downloads the data from url that was given
 def downloadData(url):
     # downloads contents
@@ -16,27 +15,25 @@ def downloadData(url):
     return info
 
 
-# logging.basicConfig(filename="errors.log", level=logging.ERROR)
-# logger = logging.getLogger("assignment2_IS211")
+logging.basicConfig(filename="errors.log", level=logging.ERROR)
+logger = logging.getLogger("assignment2_IS211")
 
 
-# Processes datafile in CSV format
-# Takes the content and processes it line by line
+# This Takes the content and processes it line by line
 def processData(files):
-    # Takes the content and process line by line#
+    # Takes the content and processes it line by line#
     data = StringIO(files)
     # reads csv file
     csv_reader = csv.reader(data, delimiter=',')
-    # the below statement will skip the first row
+
     next(csv_reader)
     dataList = []  # store's the  data from csv
     for line in csv_reader:
-        # store's data
         dataList.append(line)
     return dataList
 
 
-imageList = []  # store browsers with image extension
+imageList = []  # store browsers image
 
 
 def imageHits(dataList):
@@ -46,14 +43,70 @@ def imageHits(dataList):
     # Searches for all hits that are for an image file #
     for line in dataList:  # iterate the data in the list
         findList = re.findall('([-\w]+\.(?:jpg|gif|png))', line[0])  # extract the extension part using
-        # regular expression
 
         if len(findList) > 0:
             imageIteration += 1
             imageList.append()
 
-        count += 1  # increment counter for entry to move to the next line
+        count += 1  # increment counter
 
-    imagePercent = (imageIteration / count) * 100  # calculate percentage of images extensions
-    imagePercent = round(imagePercent, 1)  # round off to one decimal place
-    print("Requested images account for {} % of the requests".format(imagePercent))  # print the percentage
+    imagePercent = (imageIteration / count) * 100  # calculates percentage
+    imagePercent = round(imagePercent, 1)
+    print("Images will account for {} % of the requests".format(imagePercent))
+
+
+# part IV
+def browserType(imageList):
+    browserCounts = {}  # dict
+
+    browserList = []  # hold
+    for line in imageList:
+        browserType = re.findall("(?i)(firefox|msie|chrome|safari)[/\s]([\d.]+)",
+                                 line[2])
+
+        browserList.append(browserType)  # appends the regular expression
+    for browsers in browserList:
+        if browsers[0][0] not in browserCounts:
+            browserCounts[browsers[0][0]] = 1  # if the  browwer is not in dictionary add and make count
+        else:
+            browserCounts[browsers[0][0]] += 1  # if the browser is in dictionary add the number of times by one
+
+    browserT = list()  # list to hold tuples
+    for key, value in list(browserCounts.items()):
+        browserT.append((value, key))
+
+    browserT.sort(reverse=True)  # rearrange the  tuples from largest  to smallest
+
+    print("The most popular browser is {} with {} hits".format(browserT[0][1], browserT[0][0]))
+
+
+# main function that runs when
+def main():
+    """Main function"""
+
+    global csvData
+    commandParser = argparse.ArgumentParser(description="Send a ­­url parameter to the script")
+
+    commandParser.add_argument("--url", type=str, help="Link to the csv file")
+
+    args = commandParser.parse_args()
+
+    if not args.url:
+        exit()
+
+    # call downloadData function and store in csvData
+    try:
+        csvData = downloadData(args.url)
+    except:
+        # Print message and exit application if error occurs
+        print("An error has occured. Please try again")
+        exit()
+    # takes the csvData and passes  it to processData and saves it
+    browserData = processData(csvData)
+    imageHits(browserData)
+    browserType()
+
+
+# Call the main function when script runs
+if __name__ == "__main__":
+    main()
